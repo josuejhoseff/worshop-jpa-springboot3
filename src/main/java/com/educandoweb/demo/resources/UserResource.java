99e0e989a4e37d44dd0cf99a2d51fe9f11856d21
josuejhoseff <josuejhosefff@gmail.com>
2022-10-20 17:05:05 -0300

    banco de dados H2

--- src/main/java/com/educandoweb/demo/resources/UserResource.java
+++ src/main/java/com/educandoweb/demo/resources/UserResource.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.demo.entitities.User;
 
 @RestController
-@RequestMapping(value ="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "joão", "joao@gamil.com", "99999","senha");
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
